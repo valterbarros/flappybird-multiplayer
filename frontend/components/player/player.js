2c4071137fac7a0309372e8964d683fb73165e62
Valter Barros <valterinside@gmail.com>
2018-07-22 23:01:41 -0300

    master-put-code-in-eslint-formatr
    Signed-off-by: Valter Barros <valterinside@gmail.com>

--- frontend/components/player/player.js
+++ frontend/components/player/player.js
@@ -2,55 +2,55 @@
 /* eslint global-require: 0 */
 /* eslint no-underscore-dangle: 0 */
 
-import * as buzz from "buzz/dist/buzz.min";
+import * as buzz from "buzz/dist/buzz.min"
 
 export default class Player {
   constructor(id) {
-    this.id = id;
-    this.rotation = 0;
-    this.velocity = 0;
-    this.position = 180;
-    this.rendered = false;
-    this.jump = -4.6;
+    this.id = id
+    this.rotation = 0
+    this.velocity = 0
+    this.position = 180
+    this.rendered = false
+    this.jump = -4.6
     this.soundJump = new buzz.sound(
       require("components/the-game/static/sounds/sfx_wing.ogg")
-    );
+    )
   }
 
   render() {
     if (!this.rendered) {
       const html = `<div id="${
         this.id
-      }-player" class="bird animated js-flappy-player"></div>`;
-      $("#flyarea-game").append(html);
-      this.rendered = true;
+      }-player" class="bird animated js-flappy-player"></div>`
+      $("#flyarea-game").append(html)
+      this.rendered = true
     }
   }
 
   updatePlayer() {
-    this.rotation = Math.min(this.velocity / 10 * 90, 90);
+    this.rotation = Math.min(this.velocity / 10 * 90, 90)
 
     $(this._player()).css({
       transform: `rotate(${this.rotation}deg)`,
       top: this.position
-    });
+    })
   }
 
   doJump() {
-    this.velocity = this.jump;
-    this.soundJump.stop();
-    this.soundJump.play();
+    this.velocity = this.jump
+    this.soundJump.stop()
+    this.soundJump.play()
   }
 
   incrementVelocity(gravity) {
-    this.velocity += gravity;
+    this.velocity += gravity
   }
 
   incrementPosition() {
-    this.position += this.velocity;
+    this.position += this.velocity
   }
 
   _player() {
-    return $(`#${this.id}-player`);
+    return $(`#${this.id}-player`)
   }
 }
