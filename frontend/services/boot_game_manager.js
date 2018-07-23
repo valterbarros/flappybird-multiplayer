b4f417ff75194737a635e574cdd960b460459df4
Valter Barros <valterinside@gmail.com>
2018-07-22 21:37:12 -0300

    master-remove-all-fb-main-code
    Signed-off-by: Valter Barros <valterinside@gmail.com>

--- frontend/services/boot_game_manager.js
+++ frontend/services/boot_game_manager.js
@@ -1,5 +1,16 @@
 import showSplash from "./show_splash_screen"
 
+// Função para capturar o cookie para mostrar o score posteriormente
+// function getCookie(cname) {
+//   var name = cname + "=";
+//   var ca = document.cookie.split(";");
+//   for (var i = 0; i < ca.length; i++) {
+//     var c = ca[i].trim();
+//     if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+//   }
+//   return "";
+// }
+
 $(document).ready(() => {
   // TODOimport capturar o highscore pelo cookie
   // var savedscore = getCookie("highscore");
