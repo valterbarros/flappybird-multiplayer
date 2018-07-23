816f012f41af17513ad43c48b30859e119e3f3a0
Valter Barros <valterinside@gmail.com>
2018-07-22 21:32:37 -0300

    master-some-refactors
    Signed-off-by: Valter Barros <valterinside@gmail.com>

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,14 +1,14 @@
 /* eslint import/first: 0 */
 
 // frontend/packs/application.js
-import "init";
-import "./application.css";
+import "init"
+import "./application.css"
 import "consts/game_states"
 
 import "services/boot_game_manager"
 import "../actions/click_on_replay_button"
 
-import "components/the-game/fb-main";
-import "actions/add-new-player";
-import "actions/do-jump";
+import "components/the-game/fb-main"
+import "actions/add-new-player"
+import "actions/do-jump"
 import "components/player/player"
